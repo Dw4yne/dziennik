482246fe17ae8d31ef8f6c19937bfeab2ad928d1
hetman991 <k.soliszewski@gmail.com>
2018-03-06 18:53:10 +0100

    Google

--- footer.php
+++ footer.php
@@ -7,10 +7,11 @@
   <div class="container-fluid footer1">
     <div class="text-center">
 
-      <a class="btn-floating btn-lg purple-gradient footerIcon facebook rounded-circle"><i class="fa fa-facebook-square"></i></a>
-      <a class="btn-floating btn-lg purple-gradient footerIcon github marginIcon rounded-circle"><i class="fa fa-github"></i></a>
-      <a class="btn-floating btn-lg purple-gradient footerIcon twitter marginIcon rounded-circle"><i class="fa fa-twitter"></i></a>
-      <a class="btn-floating btn-lg purple-gradient footerIcon instagram marginIcon rounded-circle"><i class="fa fa-instagram"></i></a>
+      <a class="btn-floating btn-lg footerIcon facebook rounded-circle"><i class="fa fa-facebook-square"></i></a>
+      <a class="btn-floating btn-lg footerIcon github marginIcon rounded-circle"><i class="fa fa-github"></i></a>
+      <a class="btn-floating btn-lg footerIcon googlePlus marginIcon rounded-circle"><i class="fa fa-google"></i></a>
+      <a class="btn-floating btn-lg footerIcon twitter marginIcon rounded-circle"><i class="fa fa-twitter"></i></a>
+      <a class="btn-floating btn-lg footerIcon instagram marginIcon rounded-circle"><i class="fa fa-instagram"></i></a>
 
       <h5 class="h5-responsive infoFooter">KONTAKT</h5>
       <h5 class="h5-responsive infoFooter">BLOG</h5>
