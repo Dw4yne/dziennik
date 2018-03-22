7b95fbab619a71aad9d07ebaaaeb24b2d1b05573
Dw4yne <dkowalski251@gmail.com>
2018-03-22 21:21:47 +0100

    Revert "Merge branch 'master' of https://github.com/Dw4yne/dziennik"
    This reverts commit 6b990011a343b9c7ee2647af8d76d25ad231249c, reversing
    changes made to 9198a040f9ecad3af9888c99e5861701b20df519.

--- errorForm.php
+++ errorForm.php
@@ -14,21 +14,12 @@
   <link href="css/errorForm.css" rel="stylesheet">
   <!-- Czcionki -->
 </head>
-<<<<<<< HEAD
 <body>
   <section class="section">
 
     <blockquote class="blockquote bq-primary">
         <p class="bq-title">Zgłoś problem</p>
         <p>Masz jakieś pytania? Nie wahaj się skontaktować z nami bezpośrednio. Nasz zespół powróci z rozwiązaniem tak szybko jak tylko to możliwe aby Ci pomóc!</p>
-=======
-
-<section class="section">
-
-    <blockquote class="blockquote bq-danger">
-        <p class="bq-title">Zgłoś problem</p>
-        <p>Masz jakieś pytania? Nie wahaj się skontaktować się z nami bezpośrednio. Nasz zespół powróci z rozwiązaniem tak szybko jak tylko to możliwe aby Ci pomóc!</p>
->>>>>>> 47e64ecc87b4928693873cb54719a2e61f22de89
     </blockquote>
     <div class="row">
 
@@ -87,15 +78,9 @@
                 <!--Grid row-->
 
             </form>
-<<<<<<< HEAD
             <button href="main.php" type="button" class="btn btn-danger">Anuluj</button>
             <div class="center-on-small-only send">
                 <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Wyślij</a>
-=======
-
-            <div class="center-on-small-only">
-                <a class="btn btn-danger" onclick="document.getElementById('contact-form').submit();">Wyślij</a>
->>>>>>> 47e64ecc87b4928693873cb54719a2e61f22de89
             </div>
             <div class="status"></div>
         </div>
@@ -105,11 +90,7 @@
         <div class="col-md-4 col-xl-3">
             <ul class="contact-icons">
                 <li><i class="fa fa-map-marker fa-2x"></i>
-<<<<<<< HEAD
                     <a target="_blank" href="https://www.google.pl/maps/dir//TEB+Edukacja,+Dmowskiego+16A,+80-001+Gda%C5%84sk/@54.3524179,18.5816879,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd737419cd2841:0xba0daf08fce83755!2m2!1d18.6070729!2d54.3788735!3e0"><p>Gdańsk, Dmowskiego 16/A</p></a>
-=======
-                    <p>Gdańsk, Dmowskiego 16/A</p>
->>>>>>> 47e64ecc87b4928693873cb54719a2e61f22de89
                 </li>
 
                 <li><i class="fa fa-phone fa-2x"></i>
@@ -125,7 +106,6 @@
 
     </div>
 
-<<<<<<< HEAD
   </section>
 
   <!-- SCRIPTS -->
@@ -176,7 +156,4 @@ function validateForm() {
 
 
 </body>
-=======
-</section>
->>>>>>> 47e64ecc87b4928693873cb54719a2e61f22de89
 <!--Section: Contact v.2-->
